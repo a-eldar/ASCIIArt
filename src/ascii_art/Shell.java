407a5ec8e034d450ff0949db8f82047013c982cd
ofekelly <ofek.kelly@mail.huji.ac.il>
2024-02-23 01:42:06 +0200

    add Shell class

---
+++ src/ascii_art/Shell.java
@@ -0,0 +1,12 @@
+package ascii_art;
+
+public class Shell {
+
+    private char[] DEFAULT_CHAR_SET = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private int DEFAULT_RESOLUTION = 128;
+    private String DEFAULT_IMAGE_PATH = "examples/cat.jpeg";
+
+    public void run(){
+
+    }
+}
