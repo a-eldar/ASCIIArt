85a2151b97b7831702ce234c6f3f717f93104e64
ofekelly <ofek.kelly@mail.huji.ac.il>
2024-02-22 13:26:01 +0200

    add SubImgCharMatcher class

---
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -0,0 +1,20 @@
+package image_char_matching;
+
+public class SubImgCharMatcher {
+    private final char[] charset;
+
+    public SubImgCharMatcher(char[] charset) {
+
+        this.charset = charset;
+    }
+
+    public char getCharByImageBrightness(double brightness){
+
+    }
+
+    public void removeChar(char c){
+
+    }
+
+    // add method if needed -if we added need to write it in README.
+}
