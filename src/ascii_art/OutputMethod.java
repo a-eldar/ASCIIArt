6df4d50b6d5d997174fce3af3e4b6a0acd8fdc62
ofekelly <ofek.kelly@mail.huji.ac.il>
2024-03-03 21:07:46 +0200

    submission fix

--- src/ascii_art/OutputMethod.java
+++ src/ascii_art/OutputMethod.java
@@ -4,5 +4,13 @@ package ascii_art;
  * The method of output for the ASCII art.
  */
 public enum OutputMethod {
-    HTML, CONSOLE
+    /**
+     * Output method for rendering ASCII art in HTML format.
+     */
+    HTML,
+
+    /**
+     * Output method for rendering ASCII art in the console.
+     */
+    CONSOLE
 }
