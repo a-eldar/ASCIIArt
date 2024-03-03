83e78230f12285724aaf5592abf301889fecd515
Eldar Amar <eldar.amar@mail.huji.ac.il>
2024-03-03 11:26:43 +0200

    Changed inner structure by adding 3rd party - AsciiOutputGenerator - to mask the use of specifics: HTML, CONSOLE. Added the changes to README

---
+++ src/ascii_output/AsciiOutputGenerator.java
@@ -0,0 +1,61 @@
+package ascii_output;
+
+import ascii_art.AsciiArtAlgorithm;
+
+/**
+ * Generate the output of the ASCII art.
+ * The output can be anything from OutputMethod.
+ * The HTML file will be generated in the same directory as the image file.
+ * The HTML file will use the default font.
+ * The output will be generated using the specified algorithm
+ * and the specified image file.
+ * @author Eldar Amar
+ */
+public class AsciiOutputGenerator {
+    /** Default font for HTML output */
+    public static final String HTML_FONT = "Courier New";
+    /** HTML file extension */
+    public static final String HTML_EXTENSION = ".html";
+    /** Character to start file extension */
+    public static final char EXTENSION_START = '.';
+    private AsciiOutputGenerator() {
+    }
+
+    /**
+     * Generate the output of the ASCII art.
+     * @param imageFilePath The path of the image file
+     * @param outputMethod The method of output
+     * @param algo The algorithm to generate the ASCII art
+     */
+    public static void generateOutput(String imageFilePath, OutputMethod outputMethod, AsciiArtAlgorithm algo) {
+        switch (outputMethod) {
+            case HTML:
+                String path = imageFilePath.substring(0,
+                        imageFilePath.lastIndexOf(EXTENSION_START));
+                HtmlAsciiOutput htmlAsciiOutput = new HtmlAsciiOutput(path +
+                        HTML_EXTENSION, HTML_FONT);
+                htmlAsciiOutput.out(algo.run());
+                break;
+            case CONSOLE:
+                ConsoleAsciiOutput consoleAsciiOutput = new ConsoleAsciiOutput();
+                consoleAsciiOutput.out(algo.run());
+                break;
+        }
+    }
+
+    /**
+     * Change the output method.
+     * @param outputMethod The new output method. Indifferent to case.
+     * @return The new output method from OutputMethod. If the output method is not valid, return null.
+     */
+    public static OutputMethod changeOutputMethod(String outputMethod) {
+        switch (outputMethod.toLowerCase()) {
+            case "html":
+                return OutputMethod.HTML;
+            case "console":
+                return OutputMethod.CONSOLE;
+            default:
+                return null;
+        }
+    }
+}
--- src/main/BrightnessTests.java
+++ src/main/BrightnessTests.java
@@ -14,40 +14,40 @@ public class BrightnessTests {
     public static final String SPECIAL_CHARS = "~ !@#$%^&*()_+-=[]{};':,.<>?/|\\\"";
     public static final char MIN_ASCII = '0';
     public static final char MAX_ASCII = '9';
-
-    private static void testBrightnessMapGetCharByBrightness() {
-        SubImgCharMatcher charMatcher = getMatcher();
-        for (char c = MIN_ASCII; c <= MAX_ASCII; c++)
-            System.out.println(c + " " + charMatcher.getCharBrightness(c));
-        // Test cases
-        for (double i = 0; i <= 1; i += 0.0001)
-            testSingleBrightness(charMatcher, i);
-
-    }
-
-    private static void testSingleBrightness(SubImgCharMatcher charMatcher, double brightness) {
-        char c = charMatcher.getCharByImageBrightness(brightness);
-        double trueBrightness = charMatcher.getCharBrightness(c);
-        for (char a = MIN_ASCII; a <= MAX_ASCII; a++) {
-            assert Math.abs(charMatcher.getCharBrightness(a) - brightness) >= Math.abs(trueBrightness - brightness);
-            assert Math.abs(charMatcher.getCharBrightness(a) - brightness) != Math.abs(trueBrightness - brightness) || a < c;
-        }
-    }
-
-    private static SubImgCharMatcher getMatcher() {
-        char[] chars = new char[MAX_ASCII - MIN_ASCII + 1];
-        for (char c = MIN_ASCII; c <= MAX_ASCII; c++)
-            chars[c - MIN_ASCII] = c;
-        return new SubImgCharMatcher(chars);
-    }
-
-    private static void testLinearStretch() {
-
-//        SubImgCharMatcher charMatcher = new SubImgCharMatcher();
-    }
-
-    public static void main(String[] args) {
-        testBrightnessMapGetCharByBrightness();
-    }
+//
+//    private static void testBrightnessMapGetCharByBrightness() {
+//        SubImgCharMatcher charMatcher = getMatcher();
+//        for (char c = MIN_ASCII; c <= MAX_ASCII; c++)
+//            System.out.println(c + " " + charMatcher.getCharBrightness(c));
+//        // Test cases
+//        for (double i = 0; i <= 1; i += 0.0001)
+//            testSingleBrightness(charMatcher, i);
+//
+//    }
+//
+//    private static void testSingleBrightness(SubImgCharMatcher charMatcher, double brightness) {
+//        char c = charMatcher.getCharByImageBrightness(brightness);
+//        double trueBrightness = charMatcher.getCharBrightness(c);
+//        for (char a = MIN_ASCII; a <= MAX_ASCII; a++) {
+//            assert Math.abs(charMatcher.getCharBrightness(a) - brightness) >= Math.abs(trueBrightness - brightness);
+//            assert Math.abs(charMatcher.getCharBrightness(a) - brightness) != Math.abs(trueBrightness - brightness) || a < c;
+//        }
+//    }
+//
+//    private static SubImgCharMatcher getMatcher() {
+//        char[] chars = new char[MAX_ASCII - MIN_ASCII + 1];
+//        for (char c = MIN_ASCII; c <= MAX_ASCII; c++)
+//            chars[c - MIN_ASCII] = c;
+//        return new SubImgCharMatcher(chars);
+//    }
+//
+//    private static void testLinearStretch() {
+//
+////        SubImgCharMatcher charMatcher = new SubImgCharMatcher();
+//    }
+//
+//    public static void main(String[] args) {
+//        testBrightnessMapGetCharByBrightness();
+//    }
 
 }
